523c7e263b0ced2f9039be1949d920ad70997370
j-gottlieb <gessejottlieb@gmail.com>
2019-01-14 14:44:49 -0500

    Created a timer on the app component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,62 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      time: 6,
+      beep: false,
+      timer: false
+    }
+    this.handleChange = this.handleChange.bind(this)
+    this.toggleOn = this.toggleOn.bind(this)
+    this.toggleOff = this.toggleOff.bind(this)
+  }
+
+  beep() {
+    const audioCtx = new AudioContext()
+    const osc = audioCtx.createOscillator()
+    osc.type = 'sine'
+    osc.frequency.value = 440
+    osc.connect(audioCtx.destination)
+    osc.start()
+    osc.stop(.5)
+    alert('Take 5!')
+  }
+
+  componentDidMount() {
+    console.log(this.state.timer)
+    if (this.state.timer) {
+      const time = 1000 * this.state.time
+      setInterval(() => { this.beep() }, time)
+    }
+  }
+
+  toggleOn() {
+    this.setState({ timer: true })
+    console.log(this.state)
+  }
+
+  toggleOff() {
+    this.setState({ timer: false })
+    console.log(this.state)
+  }
+
+  handleChange(event) {
+    const num = parseInt(10, event.target.value)
+    this.setState({time: num})
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <React.Fragment>
+        <h1>iRest</h1>
+        <input type='number' name='time' value={this.state.time} onChange={this.handleChange}></input>
+        <button onClick={this.toggleOn}>Start Timer</button>
+        <button onClick={this.toggleOff}>Stop Timer</button>
+      </React.Fragment>
     );
   }
 }

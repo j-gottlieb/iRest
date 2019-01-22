fd96e7d2c4d5e39538700395d656be0a258a50a1
j-gottlieb <gessejottlieb@gmail.com>
2019-01-22 14:19:03 -0500

    Updates

---
+++ precache-manifest.22ae3c09807c7838d10b10306ad5b663.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1daa7f95bbbfee51875f",
+    "url": "/iRest/static/js/runtime~main.1daa7f95.js"
+  },
+  {
+    "revision": "0f4aa0c9c769bf1047a4",
+    "url": "/iRest/static/js/main.0f4aa0c9.chunk.js"
+  },
+  {
+    "revision": "9d65a0d5e5a889113337",
+    "url": "/iRest/static/js/1.9d65a0d5.chunk.js"
+  },
+  {
+    "revision": "0f4aa0c9c769bf1047a4",
+    "url": "/iRest/static/css/main.b36905ef.chunk.css"
+  },
+  {
+    "revision": "2738fcc6554eac43275967a75cbe760d",
+    "url": "/iRest/index.html"
+  }
+];
